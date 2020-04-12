0562594d9d0496b696817d135bdb127a36298a87
Amanpreet Kaur <amankaur2628@gmail.com>
2020-04-12 19:20:14 -0300

    [Aman]Code updated1

--- app/src/main/java/com/example/databindingexample/ui/ProductListActivity.java
+++ app/src/main/java/com/example/databindingexample/ui/ProductListActivity.java
@@ -18,6 +18,7 @@ import java.util.ArrayList;
 public class ProductListActivity extends AppCompatActivity {
 
     ActivityProductListBinding pBinding;
+
     private ProductAdapter productAdapter;
     private Product mProduct;
     private ArrayList<Product> productArrayList;
@@ -30,30 +31,20 @@ public class ProductListActivity extends AppCompatActivity {
 
         populateProducts();
 
-
         productAdapter = new ProductAdapter(productArrayList);
         pBinding.rvProducts.setAdapter(productAdapter);
 
     }
     public void populateProducts(){
+        productArrayList = new ArrayList<>();
 
         productArrayList.add(new Product(1,"Red Lamp", "Red colored lamp, perfect for lighting up a room and matching any red furniture.", R.drawable.red_lamp, 10.99, 4.5F));
-
         productArrayList.add(new Product(2,"Yellow Lamp", "Yellow colored lamp, perfect for lighting up a room and matching any Yellow furniture.", R.drawable.yellow_lamp, 11.99, 5.0F));
-
-        productArrayList.add(new Product(3,"Blue Coffee Mug", "Blue Coffee Mug for drinking coffee. 100% ceramic.",
-                R.drawable.blue_mug, 5.99, 3.5F));
-
-        productArrayList.add(new Product(4,"Black Baseball Hat", "Black Baseball Hat made of 100% authentic baseball hat material.",
-                R.drawable.black_hat, 20.99, 3.5F));
-
-        productArrayList.add(new Product(5,"Orange Baseball Hat", "Orange Baseball Hat made of 100% authentic baseball hat material.",
-                R.drawable.orange_hat,23.99,4.0F));
-
+        productArrayList.add(new Product(3,"Blue Coffee Mug", "Blue Coffee Mug for drinking coffee. 100% ceramic.", R.drawable.blue_mug, 5.99, 3.5F));
+        productArrayList.add(new Product(4,"Black Baseball Hat", "Black Baseball Hat made of 100% authentic baseball hat material.", R.drawable.black_hat, 20.99, 3.5F));
+        productArrayList.add(new Product(5,"Orange Baseball Hat", "Orange Baseball Hat made of 100% authentic baseball hat material.", R.drawable.orange_hat,23.99,4.0F));
         productArrayList.add(new Product(6,"Black Shirt", "Black T-Shirt made of 100% cotton. Made for males.", R.drawable.black_shirt_male, 26.99, 5.0F));
-
-        productArrayList.add(new Product(7,"Grey Fidget Spinner", "Grey Fidget Spinner. High quality bearing for long spin time. Light and portable.", R.drawable.fidget_spinner_grey, 10.00,4.5F) );
-
+        productArrayList.add(new Product(7,"Grey Fidget Spinner", "Grey Fidget Spinner. High quality bearing for long spin time. Light and portable.", R.drawable.fidget_spinner_grey, 10.00,4.5F));
     }
 
 }
